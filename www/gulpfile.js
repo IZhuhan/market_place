f90a9699c5126b8de01551b9644c213d8387ebb8
IZhuhan <zhugan.irina@gmail.com>
2017-12-20 22:42:50 +0200

    first commit

---
+++ www/gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('autoprefixer', function () {
+    gulp.src('src/css/**/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', ['autoprefixer'], function () {
+    gulp.watch('src/css/**/*.css', ['autoprefixer']);
+});
+
+gulp.task('default', ['watch']);
