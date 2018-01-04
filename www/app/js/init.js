f5140aa6cf27143239d2c9912015e48ff7ed621b
IZhuhan <zhugan.irina@gmail.com>
2018-01-04 18:26:14 +0200

    created guide.html and default-components.css with all default components and deleted extra files

--- www/app/js/init.js
+++ www/app/js/init.js
@@ -1,14 +1,15 @@
-// init slick slider
-$('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    autoplay: true,
-    arrows: false,
-    dots:true
-  });
-          
-// init form styler for select
+
 $(function (){
+    // init slick slider
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        autoplay: true,
+        arrows: false,
+        dots:true
+    });
+    
+    // init form styler for select
     $('select').styler();
 });
