2633ac64918700f13b7c5fe78539002b9b6ac01e
IZhuhan <zhugan.irina@gmail.com>
2018-01-26 22:54:57 +0200

    added changed header, inner, footer

--- www/app/js/aside.js
+++ www/app/js/aside.js
@@ -2,9 +2,10 @@ let asideBtn = $('.mobile-aside-toggle');
 let main = $('main');
 let overlay = $('.overlay');
 
-asideBtn.on('click',function(e){
+asideBtn.on('click', function (e){
     main.toggleClass('show-aside');
 });
-overlay.on('click',function(e){
+
+overlay.on('click', function (e){
     main.toggleClass('show-aside');
 });
--- www/app/js/init.js
+++ www/app/js/init.js
@@ -1,4 +1,3 @@
-
 $(function (){
     // init slick slider
     $('.multiple-items').slick({
@@ -7,22 +6,95 @@ $(function (){
         slidesToScroll: 4,
         autoplay: true,
         arrows: false,
-        dots:true
+        dots:true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    autoplay: true,
+                    arrows: false,
+                    dots:true
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    autoplay: true,
+                    arrows: false,
+                    dots:true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    arrows: false,
+                    dots:true
+                }
+            }]
     });
-    
+
     // init form styler for select
     $('select').styler();
+
+    // init modal
+    let openModalsBtns = $('[data-modal]');
+
+    openModalsBtns.on('click', function() {
+
+        let target = $(this).attr('data-target');
+        $(target).bPopup({
+            closeClass: 'close-modal'
+        });
+
+    });
+
+    // dropdown click
+    let dropdownContainer = $('[data-dropdown-container]');
+
+    dropdownContainer.on('click', function(e){
+
+        let target = $(e.target).attr('data-dropdown');
+
+        $(target).toggleClass('open');
+
+    });
+
+    // init slicknav
+    $('.header-nav').slicknav({
+        appendTo: '.top-header .container',
+        label: ''
+    });
 });
 
 // Switch grid cards (all-items-4)
-$('#grid-thumbs').on('click', function() {
-    $('.change-icon').removeClass('active');
-    $('#grid-thumbs').addClass('active');
-    $('.card.medium-card').removeClass('row-card');
-  });
-  $('#grid-list').on('click', function() {
-    $('.change-icon').removeClass('active');
-    $('#grid-list').addClass('active');
-    $('.card.medium-card').addClass('row-card');  
-  });
-  
+function myFunction() {
+    if($(window).width() < 584){
+        $('.change-icon').removeClass('active');
+        $('#grid-thumbs').addClass('active');
+        $('.card.medium-card').removeClass('row-card');
+    }
+    else {
+        $('#grid-thumbs').on('click',function(){
+            $('.change-icon').removeClass('active');
+            $('#grid-thumbs').addClass('active');
+            $('.card.medium-card').removeClass('row-card');
+        });
+
+        $('#grid-list').on('click', function(){
+            $('.change-icon').removeClass('active');
+            $('#grid-list').addClass('active');
+            $('.card.medium-card').addClass('row-card');
+        });
+    }
+}
+
+myFunction();
+
+$(window).resize(myFunction);
